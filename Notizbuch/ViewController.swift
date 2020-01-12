28481a95823f202105b83b9435304953c1760b2f
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2020-01-12 21:35:15 +0100

    Initial Commit

---
+++ Notizbuch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notizbuch
+//
+//  Created by Magnus Kruschwitz on 12.01.20.
+//  Copyright © 2020 Magnus Kruschwitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
